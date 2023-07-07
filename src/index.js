681f3f2161a2ae67d91e808bfe2ccce75c420ce1
Ivan Kritsckii <ivan.kritsckii@yandex.ru>
2023-07-07 15:14:06 +0500

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack=[]
+  let objBracket={}
+  bracketsConfig.forEach((item)=>{
+    objBracket[item[0]]=item[1]
+  })
+  for(let i=0; i<str.length; i++){
+    if(str[i]==objBracket[stack[stack.length-1]]){
+      stack.pop()
+    }
+    else{
+      stack.push(str[i])
+    }
+  }
+  if(stack.length==0){
+    return true
+  }
+  else{
+    return false
+  }
 }
